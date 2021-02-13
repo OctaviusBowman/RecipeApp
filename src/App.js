463749822283acd9afbf5ba98a752d9cff71c49e
OctaviusBowman <octaviusbowman@gmail.com>
2021-02-13 17:04:19 -0500

    HomeScreen Layout (without large screen responsiveness)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import Recipe from "./components/Recipe";
 import "./App.css";
+import Logo from "./TheKnife.svg"
 
 const App = () => {
   // I know storing values here is bad practice. I could store these values in a .env file, but I chose not to for simplicity. The chances of anyone reading this is pretty low, however if you do have fun :)
@@ -35,23 +36,32 @@ const App = () => {
   };
 
   return (
-    <div className="App container mx-auto rounded-3xl mt-5 mb-16 h-screen">
-      <form onSubmit={getSearch} className="search-form">
-        <input
-          className="search-bar"
-          placeholder="Search for food, and we'll find some recipes!"
-          type="text"
-          value={search}
-          onChange={updateSearch}
-        />
-        <button className="search-button" type="submit">
-          Search
-        </button>
-      </form>
+    <div className="App my-3 flex flex-col justify-between mx-auto w-11/12 rounded-3xl shadow-lg">
+      {/* <div className="App my-3 container-None mx-auto w-11/12 rounded-3xl"> */}
 
-      <div>
-        <h1 className="menu text-2xl text-white">The Recipe Guide</h1>
+      <h1 className="menu text-2xl text-white pl-16 pt-10 tracking-wide font-bold">The Recipe Guide
+        <img className="inline w-8 ml-5" src={Logo} alt=""/>
+      </h1>
+
+      <div className="searchWrapper flex flex-col self-center pb-32">
+        <h1 className="font-bold text-white text-6xl">What do you have a taste for?</h1>
+
+        <form onSubmit={getSearch} className="search-form flex pt-8 justify-center">
+          <input
+            className="search-bar w-9/12 py-1 pl-6 rounded focus:outline-none text-gray-500 font-semibold text-2xl placeholder-gray-500"
+            placeholder="We'll find a recipe for you!"
+            type="text"
+            value={search}
+            onChange={updateSearch}
+          />
+          <button className="search-button rounded text-white px-3 font ml-4 text-2xl font-semibold bg-blue-500 hover:bg-blue-600 outline-none" type="submit">
+            Search
+        </button>
+        </form>
+
       </div>
+
+      <div className="flexClearFix"></div>
 
       {/* <div className="recipes">
         {recipes.map(recipe => (
@@ -64,7 +74,7 @@ const App = () => {
           />
         ))}
       </div> */}
-    </div>
+    </div >
   );
 };
 
